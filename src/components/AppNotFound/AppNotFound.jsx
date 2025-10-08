8cbcb5517bc35e90164e208c80d3ce5c6bd9eb09
Hafiz <hafiz.dev24@gmail.com>
2025-10-08 17:35:32 +0600

    implemented live search functionality

---
+++ src/components/AppNotFound/AppNotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+import appErrorImg from '../../assets/App-Error.png';
+
+const AppNotFound = ({ setFilteredApps, appsData }) => {
+  return (
+    <div className='text-center py-[80px]'>
+      <img className='mx-auto' src={appErrorImg} alt="" />
+      <h1 className='text-5xl font-semibold mt-[44px] mb-4'>OPPS!! APP NOT FOUND</h1>
+      <p className='text-xl text-[#627382] mb-4'>The App you are requesting is not found on our system.  please try another apps</p>
+      <button onClick={() => setFilteredApps(appsData)} className='text-white font-semibold bg-gradient-to-r from-[#632ee3] to-[#9f62f2] py-[12px] px-[40.5px] rounded-sm'>Go Back</button>
+    </div>
+  );
+};
+
+export default AppNotFound;
--- src/pages/AllApps/AllApps.jsx
+++ src/pages/AllApps/AllApps.jsx
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import AppCard from '../../components/AppCard/AppCard';
-import { Search } from 'lucide-react';
-import { CiSearch } from 'react-icons/ci';
 import searchImg from '../../assets/search.png';
+import AppNotFound from '../../components/AppNotFound/AppNotFound';
 
 const AllApps = () => {
   const appsData = useLoaderData();
 
+  const [filteredApps, setFilteredApps] = useState(appsData);
+
+  const handleSearch = (e) => {
+    const inputValue = e.target.value.toLowerCase().trim();
+    console.log(inputValue);
+
+    const searchedApps = appsData.filter(app => app.title.toLowerCase().includes(inputValue));
+    console.log(searchedApps);
+
+    setFilteredApps(searchedApps);
+  }
+
   return (
-    <div className='max-w-[1440px] mx-auto my-20'>
-      <h1 className='text-5xl font-bold text-center'>Our All Applications</h1>
-      <p className='text-xl mt-4 mb-10 text-center'>Explore All Apps on the Market developed by us. We code for Millions</p>
-      <div className='flex justify-between mb-4'>
-        <h3 className='text-2xl font-semibold'>({appsData.length}) Apps Found</h3>
-        <label htmlFor="" className='relative'>
-          <input type="search" name="" id="" className='border border-[#d2d2d2] py-3 pl-[46px] w-[400px] rounded-sm' placeholder='search Apps' />
-          <img className='absolute top-[16px] left-[16px]' src={searchImg} alt="" />
-        </label>
-      </div>
-      <div className='grid grid-cols-4 gap-4'>
-        {
-          appsData.map(app => <AppCard key={app.id} app={app}></AppCard>)
-        }
-      </div>
-    </div>
+    filteredApps.length ?
+      (<div className='max-w-[1440px] mx-auto my-20'>
+        <h1 className='text-5xl font-bold text-center'>Our All Applications</h1>
+        <p className='text-xl mt-4 mb-10 text-center'>Explore All Apps on the Market developed by us. We code for Millions</p>
+        <div className='flex justify-between mb-4'>
+          <h3 className='text-2xl font-semibold'>({filteredApps.length}) Apps Found</h3>
+          <label htmlFor="" className='relative'>
+            <input onChange={handleSearch} type="search" name="" id="" className='border border-[#d2d2d2] py-3 pl-[46px] w-[400px] rounded-sm' placeholder='search Apps' />
+            <img className='absolute top-[16px] left-[16px]' src={searchImg} alt="" />
+          </label>
+        </div>
+        <div className='grid grid-cols-4 gap-4'>
+          {
+            filteredApps.map(app => <AppCard key={app.id} app={app}></AppCard>)
+          }
+        </div>
+      </div>)
+      :
+      (<AppNotFound setFilteredApps={setFilteredApps} appsData={appsData}></AppNotFound>)
   );
 };
 
