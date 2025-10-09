39a9ba446db7c86e94f57a0b68b9fecc42bd7b9b
Hafiz <hafiz.dev24@gmail.com>
2025-10-09 23:54:36 +0600

    implemented a dropdown for sorting apps by download count

--- src/pages/MyInstallation/MyInstallation.jsx
+++ src/pages/MyInstallation/MyInstallation.jsx
@@ -6,10 +6,10 @@ import { toast } from 'react-toastify';
 
 const MyInstallation = () => {
   const appsData = useLoaderData();
-
   const installedAppFromLS = getAppFromLS();
 
   const [filteredApps, setFilteredApps] = useState(installedAppFromLS);
+  const [sortOrder, setSortOrder] = useState('');
 
   const filteredAppsData = appsData.filter(app => filteredApps.includes(app.id));
 
@@ -23,21 +23,37 @@ const MyInstallation = () => {
     toast.error(`${uninstalledApp.title} Uninstalled from your Device`);
   }
 
+  const handleSortOrder = () => {
+    if (sortOrder === "highToLow") {
+      const sortedApps = [...filteredAppsData].sort((a, b) => b.downloads - a.downloads);
+      return sortedApps;
+    }
+    else if (sortOrder === "lowToHigh") {
+      const sortedApps = [...filteredAppsData].sort((a, b) => a.downloads - b.downloads);
+      return sortedApps;
+    }
+    else {
+      return filteredAppsData;
+    }
+  }
+
+  const sortedAppsData = handleSortOrder();
+
   return (
     <div className='max-w-[1440px] mx-auto py-20'>
       <h1 className='text-5xl font-bold text-center'>Your Installed Apps</h1>
       <p className='text-xl text-[#627382] mt-4 mb-10 text-center'>Explore All Trending Apps on the Market developed by us</p>
       <div className='flex justify-between items-center mb-4'>
-        <h3 className='text-2xl font-semibold'>{filteredAppsData.length} Apps Found</h3>
+        <h3 className='text-2xl font-semibold'>{sortedAppsData.length} Apps Found</h3>
         {/* dropdown */}
-        <select defaultValue="Pick a color" className="select">
-          <option disabled={true}>Sort By Download</option>
-          <option>High-Low</option>
-          <option>Low-High</option>
+        <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)} className='py-3 px-4 border border-[#d2d2d2] rounded-sm'>
+          <option value="" disabled={true}>Sort By Download</option>
+          <option value="highToLow">High-Low</option>
+          <option value="lowToHigh">Low-High</option>
         </select>
       </div>
       {
-        filteredAppsData.map(app => <InstalledAppsCard
+        sortedAppsData.map(app => <InstalledAppsCard
           key={app.id}
           app={app}
           handleUninstall={handleUninstall}>
